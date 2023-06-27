d714ca15a2eef1ae47b12bf7c2be6d4d28c3b479
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-06-27 12:18:24 +1000

    linghan fix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,23 +6,28 @@ import Payload from '../types/Payload'
 import Request from '../types/Request'
 
 const authenticateUser = (req: Request, res: Response, next: NextFunction): void => {
-  // Check authorization field in req.headers, e.g. authorization: 'Bearer ey...'
   const authHeader = req.headers.authorization
   if (!authHeader || !authHeader.startsWith('Bearer')) {
     throw new Error('Authentication Invalid')
   }
 
-  // Get token from authorizaiton field
   const token = authHeader.split(' ')[1]
 
-  // Check if no token
   if (!token) {
     res.status(HttpStatusCodes.UNAUTHORIZED).json({ msg: 'Authorization denied' })
     return
   }
-  // Verify token
+
   try {
-    const payload: Payload | any = jwt.verify(token, process.env.JWT_SECRET)
+    const verifiedToken = jwt.verify(token, process.env.JWT_SECRET)
+
+    let payload: Payload
+    if (typeof verifiedToken === 'string') {
+      payload = { userId: verifiedToken }
+    } else {
+      payload = verifiedToken
+    }
+
     req.userId = payload.userId
     next()
   } catch (err) {
