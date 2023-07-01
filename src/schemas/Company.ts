32717dba1fe06b5e4ba2eecffbaa5ee5f1c6c39b
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-07-01 10:49:19 +1000

    Reducing namespace pollution as using joi,and reuseable improve

--- src/schemas/Company.ts
+++ src/schemas/Company.ts
@@ -1,12 +1,14 @@
-import Joi from 'joi'
+import { object, string } from 'joi'
 
-const CompanySchema = Joi.object({
-  domain: Joi.string()
+const domainErrorMessage = 'Please provide a valid domain name ending with ".crankbit.net".'
+
+const CompanySchema = object({
+  domain: string()
     .required()
     .min(3)
     .max(30)
     .regex(/^[A-Za-z0-9_-]+\.crankbit\.net$/)
-    .error(new Error('Please provide a valid domain name ending with ".crankbit.net".')),
+    .error(new Error(domainErrorMessage)),
 })
 
 export default CompanySchema
