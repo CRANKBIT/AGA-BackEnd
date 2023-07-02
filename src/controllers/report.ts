7abe85e2db011fbcdc96fa4af2e716043e3d16d3
LingHan.S <LINGKUI0324@GMAIL.COM>
2023-07-02 10:59:46 +1000

    feat: add CRUD for report

--- src/controllers/report.ts
+++ src/controllers/report.ts
@@ -1,25 +1,40 @@
-import { Response } from 'express'
-import { StatusCodes } from 'http-status-codes'
-import { Report } from '../models/Report'
-import Request from '../types/Request'
-import ReportSchema from '../schemas/Report'
+import mongoose, { Document, Model, Schema } from 'mongoose'
 
-const createReport = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { error, value } = ReportSchema.validate(req.body)
-    if (error) {
-      console.log(error.message)
-      res.status(StatusCodes.BAD_REQUEST).json({ msg: error.message })
-      return
-    }
-
-    const report = await Report.create(value)
-
-    res.status(StatusCodes.CREATED).json({ report })
-  } catch (error) {
-    console.log(error.message)
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: error.message })
-  }
+export interface IReport extends Document {
+  title: string
 }
 
-export default { createReport }
+export interface IReportModel extends Model<IReport> {
+  createReport(reportData: Partial<IReport>): Promise<IReport>
+  getReportById(reportId: string): Promise<IReport | null>
+  updateReport(reportId: string, updatedData: Partial<IReport>): Promise<IReport | null>
+  deleteReport(reportId: string): Promise<IReport | null>
+}
+
+const ReportSchema = new Schema<IReport, IReportModel>({
+  title: {
+    type: String,
+    required: [true, 'please enter your report title.'],
+  },
+})
+
+ReportSchema.statics.createReport = async function (reportData: Partial<IReport>): Promise<IReport> {
+  return this.create(reportData)
+}
+
+ReportSchema.statics.getReportById = async function (reportId: string): Promise<IReport | null> {
+  return this.findById(reportId).lean()
+}
+
+ReportSchema.statics.updateReport = async function (
+  reportId: string,
+  updatedData: Partial<IReport>
+): Promise<IReport | null> {
+  return this.findByIdAndUpdate(reportId, updatedData, { new: true }).lean()
+}
+
+ReportSchema.statics.deleteReport = async function (reportId: string): Promise<IReport | null> {
+  return this.findByIdAndDelete(reportId).lean()
+}
+
+export const Report: IReportModel = mongoose.model<IReport, IReportModel>('Report', ReportSchema)
--- src/models/Report.ts
+++ src/models/Report.ts
@@ -1,14 +1,37 @@
-import mongoose, { Document } from 'mongoose'
+import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IReport extends Document {
-  title: string
+  title: string;
 }
 
-const ReportSchema = new mongoose.Schema({
+const ReportSchema = new Schema({
   title: {
     type: String,
     required: [true, 'please enter your report title.'],
   },
-})
+});
 
-export const Report = mongoose.model<IReport>('Report', ReportSchema)
+ReportSchema.statics.createReport = async function (reportData: Partial<IReport>): Promise<IReport> {
+  const report = await this.create(reportData);
+  return report;
+};
+
+ReportSchema.statics.getReportById = async function (reportId: string): Promise<IReport | null> {
+  const report = await this.findById(reportId);
+  return report;
+};
+
+ReportSchema.statics.updateReport = async function (
+  reportId: string,
+  updatedData: Partial<IReport>
+): Promise<IReport | null> {
+  const report = await this.findByIdAndUpdate(reportId, updatedData, { new: true });
+  return report;
+};
+
+ReportSchema.statics.deleteReport = async function (reportId: string): Promise<IReport | null> {
+  const report = await this.findByIdAndDelete(reportId);
+  return report;
+};
+
+export const Report = mongoose.model<IReport>('Report', ReportSchema);
