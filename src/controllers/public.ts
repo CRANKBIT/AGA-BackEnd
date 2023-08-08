abe7165b1c6b563fbf9f1b6690b665d1941bea21
TaoGit2023 <128274932+TaoGit2023@users.noreply.github.com>
2023-08-09 00:07:30 +1000

    Cb 169 backend 404 (#38)
    * checkDomain
    
    * backend 404
    
    * backend 404

--- src/controllers/public.ts
+++ src/controllers/public.ts
@@ -1,17 +1,26 @@
-// import { Response } from 'express'
-// import Joi from 'joi'
-// import Request from '../../types/Request'
-// import { ICompany } from '../../models/private/Company'
-// import CompanySchema from '../../schemas/Company'
+import { Response } from 'express'
+import Request from '../types/Request'
+import connectDB from '../database'
+import { privateModel } from '../models'
 
 // 进入一个不存在的company subdomain => 404 //Tao
-// export const checkCompany = async (req: Request, res: Response): Promise<void> => {
-//   try {
-//     const {subdomain} = req.body
-//     // check if this subdomian is exist in company list
+const checkSubDomain = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { domain } = req.params
+    const subdomain = process.env.MAIN_HOST || 'www'
+    console.log(domain)
+    const connection = await connectDB(subdomain)
+    req.model = privateModel(connection)
+    // check if this subdomian is in company list
 
-//     res.json(newCompany)
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal Server Error' })
-//   }
-// }
+    const result = await req.model.Company.findOne({ domain })
+    if (result) {
+      res.json(true)
+    } else {
+      res.json(false)
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
+}
+export default checkSubDomain
