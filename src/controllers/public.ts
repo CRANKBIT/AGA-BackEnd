a539e4d4e47955ed7f9cbdab1c2d7623ae29bd04
Alan <68600416+Qiming-Liu@users.noreply.github.com>
2023-08-09 14:17:52 +0930

    Update public.ts

--- src/controllers/public.ts
+++ src/controllers/public.ts
@@ -3,16 +3,16 @@ import Request from '../types/Request'
 import connectDB from '../database'
 import { privateModel } from '../models'
 
-// 进入一个不存在的company subdomain => 404 //Tao
 const checkSubDomain = async (req: Request, res: Response): Promise<void> => {
   try {
     const { domain } = req.params
+    if (domain === (process.env.MAIN_HOST || 'www')) {
+      return true
+    }
     const subdomain = process.env.MAIN_HOST || 'www'
-    console.log(domain)
     const connection = await connectDB(subdomain)
     req.model = privateModel(connection)
     // check if this subdomian is in company list
-
     const result = await req.model.Company.findOne({ domain })
     if (result) {
       res.json(true)
