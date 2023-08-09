63a896ad78f212c879acf6c17e5cee2a0afe7b25
Alan <68600416+Qiming-Liu@users.noreply.github.com>
2023-08-09 14:30:03 +0930

    fix

--- src/controllers/public.ts
+++ src/controllers/public.ts
@@ -1,26 +1,27 @@
-import { Response } from 'express'
-import Request from '../types/Request'
-import connectDB from '../database'
-import { privateModel } from '../models'
+import { Response } from "express";
+import Request from "../types/Request";
+import connectDB from "../database";
+import { privateModel } from "../models";
 
 const checkSubDomain = async (req: Request, res: Response): Promise<void> => {
   try {
-    const { domain } = req.params
-    if (domain === (process.env.MAIN_HOST || 'www')) {
-      return true
-    }
-    const subdomain = process.env.MAIN_HOST || 'www'
-    const connection = await connectDB(subdomain)
-    req.model = privateModel(connection)
-    // check if this subdomian is in company list
-    const result = await req.model.Company.findOne({ domain })
-    if (result) {
-      res.json(true)
+    const { domain } = req.params;
+    if (domain === (process.env.MAIN_HOST || "www")) {
+      res.json(true);
     } else {
-      res.json(false)
+      const subdomain = process.env.MAIN_HOST || "www";
+      const connection = await connectDB(subdomain);
+      req.model = privateModel(connection);
+      // check if this subdomian is in company list
+      const result = await req.model.Company.findOne({ domain });
+      if (result) {
+        res.json(true);
+      } else {
+        res.json(false);
+      }
     }
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' })
+    res.status(500).json({ error: "Internal Server Error" });
   }
-}
-export default checkSubDomain
+};
+export default checkSubDomain;
