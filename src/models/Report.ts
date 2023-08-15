987bb023612007d297bbe35c5b8320391be39bff
Alan <prossliu@gmail.com>
2023-08-15 23:06:02 +0930

    fix build

--- src/models/Report.ts
+++ src/models/Report.ts
@@ -1,6 +1,5 @@
 import { Document, Schema, Types } from 'mongoose'
 
-
 export interface IReport extends Document {
   Year: string
   Make: string
@@ -12,31 +11,31 @@ export interface IReport extends Document {
 }
 
 export const ReportSchema = new Schema<IReport>({
-    Year: {
-      type: String,
-      required: [true, 'Please enter the make of the vehicle.'],
-    },
-    Make: {
-      type: String,
-      required: [true, 'Please enter the model of the vehicle.'],
-    },
-    Model: {
-      type: String,
-      required: [true, 'Please enter the rego of the vehicle.'],
-    },
-    Rego: {
-      type: String,
-      required: [true, 'Please enter the year of the vehicle.'],
-    },
+  Year: {
+    type: String,
+    required: [true, 'Please enter the make of the vehicle.'],
+  },
+  Make: {
+    type: String,
+    required: [true, 'Please enter the model of the vehicle.'],
+  },
+  Model: {
+    type: String,
+    required: [true, 'Please enter the rego of the vehicle.'],
+  },
+  Rego: {
+    type: String,
+    required: [true, 'Please enter the year of the vehicle.'],
+  },
 
-    Odometer: {
-      type: String,
-      required: [true, 'Please enter the odometer of the vehicle.'],
-    },
-    CustomerName: {
-      type: String,
-      required: [true, 'Please enter the owner name of the vehicle.'],
-    },
+  Odometer: {
+    type: String,
+    required: [true, 'Please enter the odometer of the vehicle.'],
+  },
+  CustomerName: {
+    type: String,
+    required: [true, 'Please enter the owner name of the vehicle.'],
+  },
   service: {
     type: [String],
     required: [true, 'Please enter the service.'],
